9e3e6551665f4ab352bad641c6b62f9ed8d3b869
akmatoff <akmatoff.exe@gmail.com>
2025-02-06 04:58:51 +0600

    display categories and scroll to them

--- src/bar.js
+++ src/bar.js
@@ -1,16 +1,34 @@
 import { barCategories } from "./data/categories"
 import { currentLanguage, loadLanguage } from "./i18n"
 
-const barCategoriesDiv = document.querySelector('#barCategories')
+const barCategoriesSelector = document.querySelector('#barCategoriesSelector')
+const barCategoriesSection = document.querySelector('#barCategoriesContent')
 
 export function displayCousineCategories() {
   barCategories.forEach((category) => {
 
-    const categoryElement = document.createElement('div')
+    const categoryElement = document.createElement('a')
+    categoryElement.setAttribute('href', `#${category}`)
     categoryElement.setAttribute('data-i18n', `bar_categories.${category}`)
     categoryElement.classList.add('category')
 
-    barCategoriesDiv.appendChild(categoryElement);
+    barCategoriesSelector.appendChild(categoryElement);
+
+
+    const categorySection = document.createElement('div')
+    categorySection.classList.add('category-section')
+    categorySection.setAttribute('id', category)
+
+    const title = document.createElement('h1')
+    title.setAttribute('data-i18n', `bar_categories.${category}`)
+    title.classList.add('uppercase', 'text-[18px]', 'md:text-[35px]')
+
+    categorySection.appendChild(title)
+
+
+
+    barCategoriesSection.appendChild(categorySection)
+
   })
 }
 
--- src/cousine.js
+++ src/cousine.js
@@ -1,20 +1,39 @@
 import { cousineCategories } from "./data/categories"
 import { currentLanguage, loadLanguage } from "./i18n"
 
-const cousineCategoriesDiv = document.querySelector('#cousineCategories')
+// Selectors
+const cousineCategoriesSelector = document.querySelector('#cousineCategoriesSelector')
+const cousineCategoriesSection = document.querySelector('#cousineCategoriesContent')
 
-export function displayCousineCategories() {
+// Listeners
+document.addEventListener('DOMContentLoaded', () => {
+  displayCousineCategoriesSelector()
+  loadLanguage(currentLanguage)
+})
+
+// Functions
+function displayCousineCategoriesSelector() {
   cousineCategories.forEach((category) => {
 
-    const categoryElement = document.createElement('div')
+    const categoryElement = document.createElement('a')
+    categoryElement.setAttribute('href', `#${category}`)
     categoryElement.setAttribute('data-i18n', `cousine_categories.${category}`)
     categoryElement.classList.add('category')
 
-    cousineCategoriesDiv.appendChild(categoryElement);
+    cousineCategoriesSelector.appendChild(categoryElement);
+
+    const categorySection = document.createElement('div')
+    categorySection.classList.add('category-section')
+    categorySection.setAttribute('id', category)
+
+    const title = document.createElement('h1')
+    title.setAttribute('data-i18n', `cousine_categories.${category}`)
+    title.classList.add('uppercase', 'text-[18px]', 'md:text-[35px]')
+
+    categorySection.appendChild(title)
+
+    cousineCategoriesSection.appendChild(categorySection);
   })
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    displayCousineCategories()
-    loadLanguage(currentLanguage)
-})
+
