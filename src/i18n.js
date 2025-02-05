55456fa95b0bdc778db52297b7f07e00433ae309
akmatoff <akmatoff.exe@gmail.com>
2025-02-06 03:24:21 +0600

    feat: add language selectors, switch languages

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,14 @@
 import ru from './locales/ru.json';
+import en from './locales/en.json';
+import kg from './locales/kg.json'
 
 const LANGUAGE = {
     ru,
+    en,
+    kg
 }
 
-const currentLanguage = localStorage.getItem("language") || "ru";
+export let currentLanguage = localStorage.getItem("language") || "ru";
 
 function getNestedTranslation(obj, keyPath) {
     return keyPath.split('.').reduce((acc, key) => acc && acc[key], obj);
@@ -26,4 +30,9 @@ export async function loadLanguage(language) {
         console.log(e);
         console.error('Ошибка при загрузке языка')
     }
+}
+
+export function switchLanguage(language) {
+    currentLanguage = language;
+    localStorage.setItem("language", language)
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { loadLanguage } from './i18n';
+import { loadLanguage, currentLanguage, switchLanguage } from './i18n';
 import './style.css'
 
 const cousineMenuButton = document.querySelector('.cousine')
 const barMenuButton = document.querySelector('.bar')
+
+const languageSelectors = document.querySelectorAll('.language-select')
 
 cousineMenuButton.addEventListener('click', () => {
   window.location.href = '/cousine.html'
@@ -13,7 +15,33 @@ barMenuButton.addEventListener('click', () => {
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded')
-  loadLanguage('ru')
+  loadLanguage(currentLanguage)
+  updateLanguageSelectors()
 })
+
+languageSelectors.forEach((selector) => {
+
+  const language = selector.getAttribute("data-lang")
+
+  selector.addEventListener('click', () => {
+    switchLanguage(language)
+    loadLanguage(language)
+
+    updateLanguageSelectors()
+    
+  })
+})
+
+export function updateLanguageSelectors() {
+  languageSelectors.forEach((selector) => {
+  const language = selector.getAttribute("data-lang")
+    
+    
+    if (currentLanguage === language) {
+      selector.classList.add('border-[0.5px]')
+    } else {
+      selector.classList.remove('border-[0.5px]');
+    }
+  })
+}
 
